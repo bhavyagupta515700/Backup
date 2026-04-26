59ee25adf3d3d55010c853ed7034aaff90def26d
bhavyagupta515700 <guptaroshni411941@gmail.com>
2026-04-27 02:25:27 +0530

    Update page
    Added Navbar component to the layout for protected routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 // src/App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";   // ✅ ADD THIS
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
@@ -7,17 +9,66 @@ import CreatePoll from "./pages/CreatePoll";
 import CreateSurvey from "./pages/CreateSurvey";
 import SurveyPage from "./pages/SurveyPage";
 import ProtectRoutes from "./components/ProtectRoutes";
+
+/* Layout for pages WITH navbar */
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* ❌ No Navbar */}
         <Route path="/" element={<Login />} />
-         <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<ProtectRoutes><Home /></ProtectRoutes>} />
-        <Route path="/create-poll" element={<CreatePoll />} />
-        <Route path="/create-survey" element={<CreateSurvey />} />
-        <Route path="/survey/:id" element={<SurveyPage />} />
+
+        {/* ✅ WITH Navbar */}
+        <Route 
+          path="/home" 
+          element={
+            <ProtectRoutes>
+              <Layout>
+                <Home />
+              </Layout>
+            </ProtectRoutes>
+          } 
+        />
+
+        <Route 
+          path="/create-poll" 
+          element={
+            <Layout>
+              <CreatePoll />
+            </Layout>
+          } 
+        />
+
+        <Route 
+          path="/create-survey" 
+          element={
+            <Layout>
+              <CreateSurvey />
+            </Layout>
+          } 
+        />
+
+        <Route 
+          path="/survey/:id" 
+          element={
+            <Layout>
+              <SurveyPage />
+            </Layout>
+          } 
+        />
+
       </Routes>
     </BrowserRouter>
   );
